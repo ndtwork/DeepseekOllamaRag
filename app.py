e8c051e9248e85bc5b56f5545e04b65168465ddc
tú <nguyenductu.work@gmail.com>
2025-03-11 13:50:55 +0700

    1

--- app.py
+++ app.py
@@ -114,7 +114,9 @@ if uploaded_file is not None:
     retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
     # Define the LLM and the prompt
-    llm = Ollama(model="deepseek-r1:1.5b")
+    #llm = Ollama(model="deepseek-r1:1.5b")
+    llm = Ollama(model="llama3.2:3b-instruct-q8_0")
+
     prompt = """
     1. Use the following pieces of context to answer the question at the end.
     2. If you don't know the answer, just say that "I don't know" but don't make up an answer on your own.\n
